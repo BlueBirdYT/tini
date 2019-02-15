7703792c7431cc70a4ba07b9dbd5ac7299ffda19
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-15 16:01:13 +0600

    Update Tini.py

--- Tini.py
+++ Tini.py
@@ -1197,14 +1197,42 @@ async def ownerinfo(ctx):
     embed.add_field(name="Co-owner: BlueBird ❄ Froakie collector#0440", value="He coded the bot")
     await client.say(embed=embed)
 
-@client.event
-async def on_reaction_add(reaction, user):
-    ChID = '545901600435208223'
-    if reaction.message != ChID:
-        return;
-    if user.reaction.emoji == "✅":
-        Verification = discord.utils.get(user.server.roles, name="Verification")
-        await client.add_roles(user, Verification) 
+@client.command(pass_context = True)
+async def play(ctx, *, url):
+    author = ctx.message.author
+    voice_channel = author.voice_channel
+    try:
+        vc = await client.join_voice_channel(voice_channel)
+        msg = await client.say("Loading...")
+        player = await vc.create_ytdl_player("ytsearch:" + url)
+        player.start()
+        await client.say("Succesfully Loaded ur song!")
+        await client.delete_message(msg)
+    except Exception as e:
+        print(e)
+        await client.say("Reconnecting")
+        for x in client.voice_clients:
+            if(x.server == ctx.message.server):
+                await x.disconnect()
+                nvc = await client.join_voice_channel(voice_channel)
+                msg = await client.say("Loading...")
+                player2 = await nvc.create_ytdl_player("ytsearch:" + url)
+                player2.start()
+
+@client.command(pass_context = True)
+async def stop(ctx):
+    for x in client.voice_clients:
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
+
+    return await client.say("I am not playing anyting???!")
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
+async def joinvoice(ctx):
+    author = ctx.message.author
+    channel = author.voice_channel
+await client.join_voice_channel(channel)                
 
         
 client.run(os.getenv('Token'))
